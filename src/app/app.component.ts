d36fcb3ebd43c15f55cc4cbbcc3deb655cbe9a9f
felipeSanchez <hfsanchez@sura.com.co>
2024-04-19 15:48:02 -0500

    Se crea estructura en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-music';
+
+  
 }
+
+/* car:carModel={
+  brand:'ford',
+  model:'focus',
+  years:2021
+}
+
+listCars:Array<carModel>=[
+{
+  brand:'Kia',
+  model:'Picandp',
+  years:2017
+}   
+] */
+
+/* Contrato carModel
+interface carModel{
+  brand:string,
+  model:string,
+  years?:number
+}
+ */
